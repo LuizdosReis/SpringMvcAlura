ca11f281981dde76685a6305968e15350926aed7
luiz dos reis <luizdosreis@gmail.com>
2017-04-27 16:17:53 -0300

    colocando as mensagens de erro na tela

--- src/main/java/br/com/casadocodigo/loja/conf/appWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/appWebConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -19,5 +21,15 @@ public class appWebConfiguration {
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
+	
+	@Bean
+	public MessageSource messageSource(){
+		ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+		source.setBasename("/WEB-INF/message");
+		source.setDefaultEncoding("UTF-8");
+		source.setCacheSeconds(1);
+		return source;
+	}
+	
 
 }
