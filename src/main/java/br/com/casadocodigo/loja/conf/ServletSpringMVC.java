f4cf3b9820b573607ee0a1e05e41a22a49303916
Luiz dos Reis <luizdosreis@gmail.com>
2017-05-10 23:04:51 -0300

    retirando import desnecessario

--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -5,7 +5,6 @@ import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer{
